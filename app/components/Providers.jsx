e28dd138ac93cea192e8f25916592c2addfaa169
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2025-07-31 09:48:35 +0300

    Initial commit

---
+++ app/components/Providers.jsx
@@ -0,0 +1,7 @@
+'use client'
+import { SessionProvider } from 'next-auth/react'
+import { React } from 'react'
+
+export default function Providers(props) {
+  return <SessionProvider>{props.children}</SessionProvider>
+}
