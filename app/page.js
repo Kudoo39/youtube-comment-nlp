0ce215602ba1d3a673ff0c148a7cfc4befef35e0
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2025-08-02 10:00:51 +0300

    Initial layout

--- app/page.js
+++ app/page.js
@@ -3,7 +3,13 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='bg-main-grey flex flex-col min-h-screen text-black'>
-      
+      <div className='grid grid-cols-3 gap-4'> 
+        <div className='bg-main-grey min-h-screen'>
+          <p className='m-4 text-[4rem] font-bold font-mono'>YVA</p>
+          <p className='m-4 text-[1.5rem] font-normal font-mono'>YouTube Video Analysis</p>
+          <p className='ml-4 mt-2 font-bold'>By: <a href='https://github.com/Kudoo39'>Kudoo39</a></p>
+        </div>
+      </div>
     </main>
   )
 }
