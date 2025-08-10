bdde1fbfefb9af4b3824cd991b3779f97f8ce9ad
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2025-08-10 18:29:22 +0300

    Implement sign in feature using next-auth

--- app/page.js
+++ app/page.js
@@ -1,13 +1,19 @@
 import Image from 'next/image'
+import AppBar from './components/AppBar'
 
 export default function Home() {
   return (
     <main className='bg-main-grey flex flex-col min-h-screen text-black'>
-      <div className='grid grid-cols-3 gap-4'> 
+      <AppBar />
+      <div className='grid grid-cols-3 gap-4'>
         <div className='bg-main-grey min-h-screen'>
           <p className='m-4 text-[4rem] font-bold font-mono'>YVA</p>
-          <p className='m-4 text-[1.5rem] font-normal font-mono'>YouTube Video Analysis</p>
-          <p className='ml-4 mt-2 font-bold'>By: <a href='https://github.com/Kudoo39'>Kudoo39</a></p>
+          <p className='m-4 text-[1.5rem] font-normal font-mono'>
+            YouTube Video Analysis
+          </p>
+          <p className='ml-4 mt-2 font-bold'>
+            By: <a href='https://github.com/Kudoo39'>Kudoo39</a>
+          </p>
         </div>
       </div>
     </main>
